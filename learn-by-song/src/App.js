aad5bc960b4e76e0a786ed96dea28c2432357cce
Dakota Dunn <dakotadunn84@gmail.com>
2021-01-10 19:57:38 -0800

    Add Data Table
    Add Data Table from
    https://www.npmjs.com/package/react-table

--- learn-by-song/src/App.js
+++ learn-by-song/src/App.js
@@ -1,9 +1,54 @@
+import React from 'react';
 import logo from './logo.svg';
 import ReactPlayer from "react-player"
+import { useTable } from "react-table"
 import './App.css';
 
 function App() {
+  
+  const data = React.useMemo(
+     () => [
+       {
+         col1: 'Hello',
+         col2: 'World',
+       },
+       {
+         col1: 'react-table',
+         col2: 'rocks',
+       },
+       {
+         col1: 'whatever',
+         col2: 'you want',
+       },
+     ],
+     []
+   )
+ 
+   const columns = React.useMemo(
+     () => [
+       {
+         Header: 'Column 1',
+         accessor: 'col1', // accessor is the "key" in the data
+       },
+       {
+         Header: 'Column 2',
+         accessor: 'col2',
+       },
+     ],
+     []
+   )
+ 
+   const {
+     getTableProps,
+     getTableBodyProps,
+     headerGroups,
+     rows,
+     prepareRow,
+   } = useTable({ columns, data })
+  
   return (
+    
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -23,6 +68,50 @@ function App() {
         >
           Learn React
         </a>
+        <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+       <thead>
+         {headerGroups.map(headerGroup => (
+           <tr {...headerGroup.getHeaderGroupProps()}>
+             {headerGroup.headers.map(column => (
+               <th
+                 {...column.getHeaderProps()}
+                 style={{
+                   borderBottom: 'solid 3px red',
+                   background: 'aliceblue',
+                   color: 'black',
+                   fontWeight: 'bold',
+                 }}
+               >
+                 {column.render('Header')}
+               </th>
+             ))}
+           </tr>
+         ))}
+       </thead>
+       <tbody {...getTableBodyProps()}>
+         {rows.map(row => {
+           prepareRow(row)
+           return (
+             <tr {...row.getRowProps()}>
+               {row.cells.map(cell => {
+                 return (
+                   <td
+                     {...cell.getCellProps()}
+                     style={{
+                       padding: '10px',
+                       border: 'solid 1px gray',
+                       background: 'papayawhip',
+                     }}
+                   >
+                     {cell.render('Cell')}
+                   </td>
+                 )
+               })}
+             </tr>
+           )
+         })}
+       </tbody>
+     </table>
       </header>
     </div>
   );
