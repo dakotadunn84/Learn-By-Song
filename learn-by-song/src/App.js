3da13cde777a9aa79cf36524e903115cc9a4c5ca
Dakota Dunn <dakotadunn84@gmail.com>
2021-01-10 18:02:31 -0800

    Project Init
    Project Init Description

--- learn-by-song/src/App.js
+++ learn-by-song/src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Learn By Song
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
