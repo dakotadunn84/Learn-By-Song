f0c0007d9efa256898981a80898a2957a47871fe
Dakota Dunn <dakotadunn84@gmail.com>
2021-11-02 18:20:30 -0700

    Updates

---
+++ precache-manifest.501bfcbf9782a0cdc58b9a8e3adca97e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7afa576b098f5fab54336b80d9e65fe5",
+    "url": "/Learn-By-Song/index.html"
+  },
+  {
+    "revision": "e473980e9a0a0e94ebb3",
+    "url": "/Learn-By-Song/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a72f3d2cc3a065d75c88",
+    "url": "/Learn-By-Song/static/js/2.3a5e0623.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/Learn-By-Song/static/js/2.3a5e0623.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f1fd81f7c957b790ebc",
+    "url": "/Learn-By-Song/static/js/3.70346783.chunk.js"
+  },
+  {
+    "revision": "e473980e9a0a0e94ebb3",
+    "url": "/Learn-By-Song/static/js/main.c3693640.chunk.js"
+  },
+  {
+    "revision": "06d209eb81869c8a9ecb",
+    "url": "/Learn-By-Song/static/js/runtime-main.d046e123.js"
+  },
+  {
+    "revision": "d8b13f1aac9062acce2c7371ba62ff48",
+    "url": "/Learn-By-Song/static/media/monkey.d8b13f1a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Learn-By-Song/precache-manifest.a0b8e1ed949124bac3e9b044ee312cd8.js"
+  "/Learn-By-Song/precache-manifest.501bfcbf9782a0cdc58b9a8e3adca97e.js"
 );
 
 self.addEventListener('message', (event) => {
